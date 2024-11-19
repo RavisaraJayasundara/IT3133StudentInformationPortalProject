17c8fb3d1a48ade9b49ac3e8a0f357c748fcf762
Ravisara Jayasundara <139541406+RavisaraJayasundara@users.noreply.github.com>
2024-11-19 15:03:35 +0000

    spell mistake

--- studentapp/src/components/StudentTable.js
+++ studentapp/src/components/StudentTable.js
@@ -2,7 +2,7 @@ import '../assests/css/compo.css';
 export default function StudentTable(props){
     return(
        <div className="outerDiv">
-           <div className="leftDiv"></div>
+           <div className="leftDiv">
            <table>
             <thead>
                 <tr>
@@ -12,7 +12,20 @@ export default function StudentTable(props){
                     <td>Country</td>
                 </tr>
             </thead>
+            <tbody>
+                {
+                    props.students.map( student =>
+                       <tr>
+                        <td>{student.firstname}</td>
+                        <td>{student.lastname}</td>
+                        <td>{student.course}</td>
+                        <td>{student.address.country}</td>
+                       </tr>
+                    )
+                }
+            </tbody>
            </table>
+           </div>
        </div>
     );
 }
